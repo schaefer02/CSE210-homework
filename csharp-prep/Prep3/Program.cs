9ecaaf18e6869391ab5241b4333367808876266d
Isaac Schaefer <IsaacTschaefer@gmail.com>
2023-06-10 20:14:57 -0500

    prep 3 guess number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int yourguess = -1;
+
+        while (yourguess != number)
+        {
+            Console.WriteLine("Guess a number between 1 and 100");
+            yourguess = int.Parse(Console.ReadLine());
+
+
+            if (number > yourguess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < yourguess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("congrats, you guessed it!");
+            }
+        }
     }
 }
