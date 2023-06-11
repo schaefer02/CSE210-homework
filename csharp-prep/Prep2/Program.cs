fa8d5e38bc051c86821c6fef873d33863a82e7a0
Isaac Schaefer <IsaacTschaefer@gmail.com>
2023-06-10 19:27:09 -0500

    prep 2 grade program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade?");
+        string  gradeFromUser = Console.ReadLine();
+        int x = int.Parse(gradeFromUser);
+
+        string grade = "";
+        
+
+        if (x >= 90)
+        {
+            grade = "A";
+        }
+        else if (x >= 80)
+        {
+            grade = "B";
+        }
+        else if (x >= 70)
+        {
+            grade = "C";
+        }
+        else if (x >= 60)
+        {
+            grade = "D";
+        }
+        else 
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (x > 70)
+        {
+            Console.WriteLine("Congrats, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
