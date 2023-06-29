7c086e13d86f93001d0e8113c90aef73eb854982
Isaac Schaefer <IsaacTschaefer@gmail.com>
2023-06-29 15:50:06 -0500

    Activity4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+       Assignment as1 = new Assignment("Samuel Bennet","Multiplication");
+       Console.WriteLine(as1.GetSummary());
+
+       MathAssignment as2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3","8-19");
+       Console.WriteLine(as2.GetSummary());
+       Console.WriteLine(as2.GetHomeworkList());
+
+       WritingAssignment as3 = new WritingAssignment("Mary Waters", "European History","The Causes of World War II by Mary Waters");
+       Console.WriteLine(as3.GetSummary());
+       Console.WriteLine(as3.GetWritingInfo());
     }
+
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,18 @@
+
+public class WritingAssignment : Assignment
+{
+    public string _title;
+
+    public WritingAssignment(string studentName, string topic ,string title) : base(studentName,topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInfo()
+    {
+        string studentName = GetStudent();
+        
+        return $"{_title} - {studentName}";
+    }
+
+}
