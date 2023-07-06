edc8c6576ded6c243883a59986fa194220c9db8d
Isaac Schaefer <IsaacTschaefer@gmail.com>
2023-07-06 16:17:34 -0500

    learning w5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        Circle s2 = new Circle("Blue", 5);
+        shapes.Add(s2);
+
+        Rectangle s3 = new Rectangle("Pink", 3, 2);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has and area of {area}.");
+
+        }
+        
     }
 }
